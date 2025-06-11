258d1e7efdc56c91cdf56e826e86fd2a9700ea00
sumitnjmsingh <sumitsamsingh1111@gmail.com>
2025-06-11 22:55:03 +0530

    update gradient and header components, add mobile menu bar to header

--- components/common/gradient.tsx
+++ components/common/gradient.tsx
@@ -16,7 +16,7 @@ export default function Gradient({
             clipPath:
               "polygon(50% 0%,61% 35%,98% 35%,68% 57%,79% 91%,50% 70%, 21% 91%,32% 57%,2% 35%,39% 35%)",
           }}
-          className="relative left-[calc(50%-11rem)] aspect-1155/678 w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-br from-neutral-900 via-neutral-800 to-neutral-700 opacity-20 sm:left-[calc(50%-30rem)] sm:w-[72rem]"
+          className="relative left-[calc(50%-11rem)] aspect-1155/678 w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-br from-neutral-900 via-neutral-800 to-neutral-700 opacity-40 sm:left-[calc(50%-30rem)] sm:w-[72rem]"
         />
       </div>
       {children}
--- components/common/header.tsx
+++ components/common/header.tsx
@@ -1,31 +1,71 @@
+"use client";
 import NavLink from "./navlinks";
+import Link from "next/link";
 import { AirVent } from "lucide-react";
+import { useState } from "react";
+import { Menu, X } from "lucide-react";
 
 export default function Header() {
   const isLoggedIn = false;
+  const [isOpen, setIsOpen] = useState(false);
   return (
-    <nav className="container flex items-center justify-between py-4 lg:px-8 px-2 mx-auto">
+    <header className="flex justify-between    text-black items-center px-6 py-2 md:px-8 md:py-2 sticky top-0 z-20">
       <div className="flex lg:flex-1">
         <NavLink href="/" className="flex items-center gap-1 lg:gap-2 shrink-0">
-          <AirVent className=" h-4 lg:w-6 lg:h-6 text-gray-900 hover:rotate-12 transform transition duration-200 ease-in-out" />
-          <span className="font-extrabold lg:text-lg text-gray-900">
-            GenZilla
-          </span>
+          <AirVent className=" h-4 lg:w-6 lg:h-6 text-black hover:rotate-12 transform transition duration-200 ease-in-out z-20" />
+          <span className="font-extrabold lg:text-lg text-black z-20">GenZilla</span>
         </NavLink>
       </div>
-      <div className="flex lg:justify-center gap-4 lg:gap-12 lg:items-center">
-        {!isLoggedIn ? (
-          <NavLink href="/#pricing">Pricing</NavLink>
+
+      <nav className="hidden md:flex gap-6 font-medium md:items-center">
+        <NavLink href="#" className="text-black">
+          Home
+        </NavLink>
+        <NavLink href="#" className="text-black ">
+          Pricing
+        </NavLink>
+        <div className="bg-black/90 text-white px-4 py-1.5 rounded-full font-semibold inline-block mx-auto w-24 text-center">
+          {isLoggedIn ? (
+            <div className="flex gap-2 items-center">
+              <div>User</div>
+            </div>
+          ) : (
+            <div>
+              <Link href="/sign-in">SignIn</Link>
+            </div>
+          )}
+        </div>
+      </nav>
+
+      <button
+        className="md:hidden"
+        onClick={() => setIsOpen(!isOpen)}
+        aria-label="Toggle Menu"
+      >
+        {isOpen ? (
+          <X className="w-6 h-6 text-black bg-white " />
         ) : (
-          <NavLink href="/dashboard">Your GenZilla</NavLink>
+          <Menu className="w-6 h-6 text-black bg-white" />
         )}
-      </div>
-      <div className="flex lg:justify-end lg:flex-1">
-        
-          <div>
-            <NavLink href="/sign-in">SignIn</NavLink>
+      </button>
+
+      {isOpen && (
+        <div className="absolute top-full left-0 w-full bg-[#24243e] text-center py-4 flex flex-col gap-4 z-30">
+          <NavLink href="#">Home</NavLink>
+          <NavLink href="#">Pricing</NavLink>
+          <div className="bg-black/90 text-white px-4 py-1.5 rounded-full font-semibold inline-block text-center mx-auto w-24">
+            {isLoggedIn ? (
+              <div className="flex gap-2 items-center">
+                <div>User</div>
+              </div>
+            ) : (
+              <div>
+                <Link href="/sign-in">SignIn</Link>
+              </div>
+            )}
           </div>
-      </div>
-    </nav>
+        </div>
+      )}
+    </header>
   );
 }
